87a989acf52bfa8bf1290ab938fec8e736ab93d0
Chris Alencar <chrisalencar@hotmail.co.uk>
2017-03-19 18:13:31 +0000

    Add files via upload

--- Chris_PreSchoolApp/Chris_PreSchoolApp/ViewController.swift
+++ Chris_PreSchoolApp/Chris_PreSchoolApp/ViewController.swift
@@ -14,34 +14,38 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
-    
     @IBOutlet weak var congratsView: UIView!
-    
     @IBOutlet weak var correctLabel: UILabel!
     @IBOutlet weak var incorrectLabel: UILabel!
-    
     @IBOutlet weak var starImage: UIImageView!
     @IBOutlet weak var starImage2: UIImageView!
     @IBOutlet weak var thumbs1: UIImageView!
     @IBOutlet weak var thumbs2: UIImageView!
     @IBOutlet weak var thumbs3: UIImageView!
     
-    
-    
-    @IBAction func playAgain(_ sender: AnyObject) {
-        self.viewDidLoad()
-        //audioPlayer.play
-    }
-    
     var ans = 0
     var rand1 = Int(arc4random_uniform(5))
     var rand2 = Int(arc4random_uniform(5))
     var sum = 0
-    var sound = AVAudioPlayer()
+    var bgSound: AVAudioPlayer?
     var check = 0
     var counter = 1
     var timer = Timer()
-    //let url = Bundle.main.url(forResource: "win", withExtension: "mp3")!
+    
+    @IBAction func reset(_ sender: AnyObject) {
+        incorrectLabel.alpha = 0
+        congratsView.isHidden = true
+        answerLabel.textColor = UIColor.red;
+        ans = 0
+        rand1 = Int(arc4random_uniform(5))
+        rand2 = Int(arc4random_uniform(5))
+        sum = rand1 + rand2
+        questionLabel.text = String(rand1) + " + " + String(rand2) + " = "
+        
+        
+        
+        
+    }
     
     @IBAction func button(_ sender: AnyObject) {
         ans = sender.tag
@@ -55,8 +59,6 @@ class ViewController: UIViewController {
         }
         else {
             UIView.animate(withDuration: 0.8, animations: {
-                self.incorrectLabel.alpha = 1
-                self.incorrectLabel.alpha = 0
                 self.incorrectLabel.alpha = 1
                 self.incorrectLabel.alpha = 0
             }, completion: nil)
@@ -87,32 +89,21 @@ class ViewController: UIViewController {
             self.thumbs3.center.x = 500
             }, completion: nil)
         
-        func playSound(filename: String) {
-            let url = Bundle.main.url(forResource: "win", withExtension: "mp3")
-            guard let newURL = url else {
-                print ("Could not find file: \(filename)")
-                return
-            }
-            do {
-                sound = try AVAudioPlayer(contentsOf: newURL)
-                sound.numberOfLoops = -1
-                sound.prepareToPlay()
-                sound.play()
-            } catch let error as NSError {
-                print (error.description)
-            }
+        let url = Bundle.main.path(forResource: "relaxing", ofType: "mp3")
+        do {
+            bgSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: url!))
+            bgSound?.numberOfLoops = -1
+            bgSound?.prepareToPlay()
+            bgSound?.play()
         }
-        
-        //if (check == 0) {
-            //do {
-                //sound = try AVAudioPlayer(contentsOf: url)
-                //guard let sound = sound else {return}
-            //}
+        catch let error as NSError {
+            print (error.description)
+        }
     }
-        
-        override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
