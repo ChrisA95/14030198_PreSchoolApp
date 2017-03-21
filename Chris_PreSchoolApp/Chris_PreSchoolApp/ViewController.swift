4bbc577ceb31b7c4e987154815212cb33ca82be6
Chris Alencar <chrisalencar@hotmail.co.uk>
2017-03-21 23:26:45 +0000

    Added loading screen, background image and correct/incorrect sounds

--- Chris_PreSchoolApp/Chris_PreSchoolApp/ViewController.swift
+++ Chris_PreSchoolApp/Chris_PreSchoolApp/ViewController.swift
@@ -10,30 +10,80 @@ import UIKit
 
 import AVFoundation
 
+var bgMusic = AVAudioPlayer()
+var bgMusicSession = AVAudioSession.sharedInstance()
+var correctSound = AVAudioPlayer()
+var incorrectSound = AVAudioPlayer()
+
+func playbgMusic(filename: String) {
+    let url = Bundle.main.path(forResource: "relaxing", ofType: "mp3")
+    do {
+        bgMusic = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: url!))
+        bgMusic.setVolume(0.5, fadeDuration: 0)
+        bgMusic.numberOfLoops = -1
+        bgMusic.prepareToPlay()
+        bgMusic.play()
+        do {
+            try bgMusicSession.setCategory(AVAudioSessionCategoryAmbient)
+        }
+    }
+    catch let error as NSError {
+        print (error.description)
+    }
+}
+
+func playCorrectSound(filename: String) {
+    let url = Bundle.main.path(forResource: "chime", ofType: "mp3")
+    do {
+        correctSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: url!))
+        correctSound.prepareToPlay()
+        correctSound.play()
+    }
+    catch let error as NSError {
+        print (error.description)
+    }
+}
+
+func playIncorrectSound(filename: String) {
+    let url = Bundle.main.path(forResource: "LowBeep", ofType: "mp3")
+    do {
+        incorrectSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: url!))
+        incorrectSound.setVolume(1.2, fadeDuration: 0)
+        incorrectSound.prepareToPlay()
+        incorrectSound.play()
+    }
+    catch let error as NSError {
+        print (error.description)
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
+        
+    
     @IBOutlet weak var congratsView: UIView!
     @IBOutlet weak var correctLabel: UILabel!
-    @IBOutlet weak var incorrectLabel: UILabel!
     @IBOutlet weak var starImage: UIImageView!
     @IBOutlet weak var starImage2: UIImageView!
+    @IBOutlet weak var starImage3: UIImageView!
+    @IBOutlet weak var starImage4: UIImageView!
+    @IBOutlet weak var starImage5: UIImageView!
     @IBOutlet weak var thumbs1: UIImageView!
     @IBOutlet weak var thumbs2: UIImageView!
     @IBOutlet weak var thumbs3: UIImageView!
     
+    var applesReset = DraggedImageView()
     var ans = 0
     var rand1 = Int(arc4random_uniform(4))
     var rand2 = Int(arc4random_uniform(4))
     var sum = 0
     var bgSound: AVAudioPlayer?
     var check = 0
-    var counter = 1
-    var timer = Timer()
     
     @IBAction func reset(_ sender: AnyObject) {
-        incorrectLabel.alpha = 0
+        answerLabel.alpha = 0
         congratsView.isHidden = true
         answerLabel.textColor = UIColor.red;
         ans = 0
@@ -45,18 +95,39 @@ class ViewController: UIViewController {
     
     @IBAction func button(_ sender: AnyObject) {
         ans = sender.tag
+        
         if ans == sum {
+            playCorrectSound(filename: "chime.wav")
             congratsView.isHidden = false
-            correctLabel.text = String(rand1) + " + " + String(rand2) + " = " + String(sum)
+            correctLabel.text = String(rand1) + " + " + String(rand2) + " = " + String(sum) + "!"
+            
             UIView.animate(withDuration: 0.5, delay: 0, options: [.autoreverse, .repeat], animations: {
                 self.starImage.alpha = 0
                 self.starImage2.alpha = 0
+                self.starImage3.alpha = 0
+                self.starImage4.alpha = 0
+                self.starImage5.alpha = 0
+            }, completion: nil)
+            
+            UIView.animate(withDuration: 20, delay: 0, options: .repeat, animations: {
+                self.thumbs1.center.x = 500
+            }, completion: nil)
+            
+            UIView.animate(withDuration: 20, delay: 0, options: .repeat, animations: {
+                self.thumbs2.center.x = 500
+            }, completion: nil)
+            
+            UIView.animate(withDuration: 20, delay: 0, options: .repeat, animations: {
+                self.thumbs3.center.x = 500
             }, completion: nil)
         }
         else {
+            playIncorrectSound(filename: "LowBeep.mp3")
+            
             UIView.animate(withDuration: 0.8, animations: {
-                self.incorrectLabel.alpha = 1
-                self.incorrectLabel.alpha = 0
+                self.answerLabel.alpha = 1
+                self.answerLabel.alpha = 0
+                self.answerLabel.alpha = 1
             }, completion: nil)
         }
     }
@@ -64,7 +135,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        incorrectLabel.alpha = 0
+        playbgMusic(filename: "relaxing.mp3")
+        
+        //answerLabel.alpha = 0
         congratsView.isHidden = true
         answerLabel.textColor = UIColor.red;
         ans = 0
@@ -72,34 +145,10 @@ class ViewController: UIViewController {
         rand2 = Int(arc4random_uniform(5))
         sum = rand1 + rand2
         questionLabel.text = String(rand1) + " + " + String(rand2) + " = "
-
-        UIView.animate(withDuration: 30, delay: 0, options: .repeat, animations: {
-            self.thumbs1.center.x = 500
-            }, completion: nil)
-        
-        UIView.animate(withDuration: 40, delay: 0, options: .repeat, animations: {
-            self.thumbs2.center.x = 500
-            }, completion: nil)
-        
-        UIView.animate(withDuration: 50, delay: 0, options: .repeat, animations: {
-            self.thumbs3.center.x = 500
-            }, completion: nil)
-        
-        let url = Bundle.main.path(forResource: "relaxing", ofType: "mp3")
-        do {
-            bgSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: url!))
-            bgSound?.numberOfLoops = -1
-            bgSound?.prepareToPlay()
-            bgSound?.play()
-        }
-        catch let error as NSError {
-            print (error.description)
-        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 }
